84585fd8dc0c4ef35cf0b75945af214e3b4e7986
Renee Geffre <reneegeffre@RGMacBook.local>
2017-08-05 12:30:28 -0400

    updates

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+	//Declare required variables
+
+	//create an array based on my choice of theme and save it to a variable called topics
+	var topics ["Superman", "Batman", "Wolverine", "Iron Man", "Captain America", "Spiderman"];
+    
     
 
 
