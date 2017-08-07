b7d838e5fb61e1e70ead85a26b5cc7288cfe7033
Renee Geffre <reneegeffre@RGMacBook.local>
2017-08-07 18:53:00 -0400

    udpates

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,9 +1,26 @@
-	//Declare required variables
+// Declare required variables
 
-	//creates an array based on comic book and movie Superheros and saves it to an array called topics
-	var topics = ["Superman", "Batman", "Wolverine"];
+// creates an array based on comic book and movie Superheros and saves it to an array called topics
+var topics = ['Superman', 'Batman', 'Wolverine']
 
-	//Loop through the array of topics
-    for (var i = 0; i < topics.length; i++);
-    	console.log(topics);
-    	
+// Function to display the buttons
+function displayButtons () {
+// Deleting button contents before adding new buttons to avoid duplicates
+  $('hero-buttons').empty()
+  for (var i = 0; i < topics.length; i++) {
+    var a = $('<button>')
+    a.text(topics[i])
+    $('#hero-buttons').append(a)
+  }
+}
+$('#add-hero').on('click', function (event) {
+  	event.preventDefault()
+// capture the input from the textbox
+  	var newHero = $('#superhero-input').val().trim()
+  	console.log(newHero)
+// add new content to the array
+  	topics.push(newHero)
+  	console.log(topics)
+})
+
+displayButtons()
