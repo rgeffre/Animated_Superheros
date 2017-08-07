394f54fcada21072dd873c7ccc4e69fdf9da0221
Renee Geffre <ReneeGeffre@RGMAC.local>
2017-08-07 01:19:51 -0400

    updates

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,14 +1,9 @@
-$(document).ready(function() {
-
 	//Declare required variables
 
-	//create an array based on my choice of theme and save it to a variable called topics
-	var topics ["Superman", "Batman", "Wolverine", "Iron Man", "Captain America", "Spiderman"];
-    
-    
+	//creates an array based on comic book and movie Superheros and saves it to an array called topics
+	var topics = ["Superman", "Batman", "Wolverine"];
 
-
-
-
-    
-}
+	//Loop through the array of topics
+    for (var i = 0; i < topics.length; i++);
+    	console.log(topics);
+    	
