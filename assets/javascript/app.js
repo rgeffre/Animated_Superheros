09cf4a3072e28248e2aa70914e43234dd158d0f1
reneegeffre <renee.geffre@gmail.com>
2017-09-21 00:54:04 -0400

    fixed issues with code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,47 @@
   // Creating initial array of Superheros
  var heroes = ['Superman', 'Batman', 'Wolverine'];
+//displayHero function displays the content retrieved from the AJAX request
+  function displayHero() {
+    //Creating the variable for the query
+    var hero = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=c3a6b1024d4240b6a7810e358792f08f&q=" + hero + "&limit=10&rating=G";
+
+    // Creating the AJAX get request
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+      //callback response after the AJAX request comes back
+    }).done(function(response) {
+      console.log(queryURL);
+
+      //saving the image url property
+      var results = response.data;
+
+      //Looping through each result item
+      for (var i = 0; i < results.length; i++) {
+        //creating a div to hold the images
+        var heroDiv = $("<div class='heroes'>");
+
+        //Creating a paragraph tag for the results rating
+        var p = $("<p>").text('Rating: ' + results[i].rating);
+
+        //creating an image tag
+        var heroImage = $('<img>');
+
+        //Setting the heroImage src attributes
+        heroImage.attr("src", results[i].images.fixed_height.url);
+        heroImage.attr("alt", "superheroes");
+
+        //Appending the paragraph and images to the heroDiv
+        heroDiv.append(p);
+        heroDiv.append(heroImage);
+
+        //Prepending the heroDiv to the #heroes-view div in the HTML
+        $("#heroes-view").prepend(heroDiv);
+      }
+
+    });
+  }
 
   // Function for displaying hero data
   function renderButtons() {
@@ -11,7 +53,7 @@
     // Looping through the array of heroes
     for (var i = 0; i < heroes.length; i++) {
 
-      // Using jQuery and dynamicaly generating buttons for each hero in the array
+      // Using jQuery and dynamically generating buttons for each hero in the array
       var a = $("<button>");
       // Adding a class of hero to our button
       a.addClass("hero");
@@ -33,49 +75,11 @@
     // Adding the hero from the textbox to our array
     heroes.push(hero);
 
-      // Calling renderButtons which handles the processing of the array
-      renderButtons();
-    });
+    // Calling renderButtons which handles the processing of the array
+    renderButtons();
+  });
 
-  //displayHero function displays the content retrieved from the AJAX request
-  function displayHero() {
-    //Creating the variable for the query
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?api_key=c3a6b1024d4240b6a7810e358792f08f&q=' +
-        hero + '&limit=10&rating=g';
-
-    // Creating the AJAX get request
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-      //callback response after the AJAX request comes back
-    }).done(function(response) {
-      console.log(response);
-      //saving the image url property
-      var imageURL = response.data.image_original_url;
-
-      //creating a div to hold the images
-      var heroDiv = $("<div class='heroes'>");
-
-      //Creating a paragraph tag for the results rating
-      var p = $("<p>").text('Rating: ' + rating);
-
-      //creating an image tag
-      var heroImage = $('<img>');
-
-      //Setting the heroImage src attributes
-      heroImage.attr("src", imageUrl);
-      heroImage.attr("alt", "superheroes");
-
-      //Appending the paragraph and images to the heroDiv
-      heroDiv.append(p);
-      heroDiv.append(heroImage);
-
-      //Prepending the heroDiv to the #heroes-view div in the HTML
-      $("#heroes-view").prepend(heroDiv);
-    });
-  }
-
-    $(document).on("click", ".hero", renderButtons);
+    $(document).on("click", ".hero", displayHero);
 
   // Calling renderButtons which handles the processing of the array
   renderButtons();
